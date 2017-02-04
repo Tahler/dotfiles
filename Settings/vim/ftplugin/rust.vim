f627d3efcc0fe9765587aef0729357ddb91eeda6
Tyler Berry <tyler@berryac.com>
2017-02-03 20:04:17 -0700

    Include file specific settings

---
+++ Settings/vim/ftplugin/rust.vim
@@ -0,0 +1,13 @@
+set hidden
+let g:racer_cmd = "$HOME/.cargo/bin/racer"
+let g:racer_experimental_completer = 1
+
+setlocal omnifunc=racer#RacerComplete
+
+nmap gd <Plug>(rust-def)
+nmap gs <Plug>(rust-def-split)
+nmap gx <Plug>(rust-def-vertical)
+nmap <leader>gd <Plug>(rust-doc)
+
+set colorcolumn=100 " Show column line at 100
+
--- Settings/vim/install.sh
+++ Settings/vim/install.sh
@@ -10,16 +10,24 @@ fi
 
 # Link `.vimrc`
 rc_file="$HOME/.vimrc"
-if [ -f "$rc_file" ] ; then
-  rm "$rc_file"
+if [ -a "$rc_file" ] ; then
+  rm -r "$rc_file"
 fi
 ln "${SCRIPT_PATH}/.vimrc" "$rc_file"
+
+# Link file specific settings
+# Note: this is destructive - removes `~/.vim/ftplugin` directory if it exists
+ftplugin_dir="$HOME/.vim/ftplugin"
+if [ -a "$ftplugin_dir" ] ; then
+  rm -r "$ftplugin_dir"
+fi
+ln -s "${SCRIPT_PATH}/ftplugin" "$ftplugin_dir"
 
 # Link color scheme
 # Note: this is destructive - removes `~/.vim/colors` directory if it exists
 colors_dir="$HOME/.vim/colors"
-if [ -f "$colors_dir" ] ; then
-  rm "$colors_dir"
+if [ -a "$colors_dir" ] ; then
+  rm -r "$colors_dir"
 fi
 ln -s "${SCRIPT_PATH}/colors" "$colors_dir"
 
