02be2118ff595ca3dbca91d07a46e14fa630f573
Tyler Berry <tyler@berryac.com>
2017-03-04 11:59:08 -0700

    Fish: Uninstalled

--- fish/.config/fish/config.fish
+++
@@ -1,9 +0,0 @@
-set PATH $HOME/bin $PATH
-set -x LANG 'en_US.UTF-8'
-setenv EDITOR vim
-
-alias mkdir='mkdir -p'
-alias shutdown='shutdown now'
-alias uptime='uptime -p'
-alias tree='tree -FC'
-alias redshift "redshift -l 40.7604900:-111.8658010 &"
