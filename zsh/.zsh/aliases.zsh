9a10a56897a9658356f723b2f4c009e0aa4f7227
Tyler Berry <tyler@berryac.com>
2017-10-02 20:27:56 -0700

    Git/Zsh: Remove many git aliases in favor of zsh

--- zsh/.zsh/aliases.zsh
+++ zsh/.zsh/aliases.zsh
@@ -1,3 +1,16 @@
+alias l='ls -CF'
+alias la='ls -A'
+alias ll='ls -alF'
+alias ls='ls --color=auto'
+
 alias vi=vim
-alias shutdown="\shutdown now"
-alias rm="trash-put"
+
+alias gcd='cd $(git rev-parse --show-toplevel)'
+alias gcob='git checkout -b'
+alias gdt='git difftool'
+
+alias grep='grep --color=auto'
+alias egrep='egrep --color=auto'
+alias fgrep='fgrep --color=auto'
+
+alias shutdown='\shutdown now'
