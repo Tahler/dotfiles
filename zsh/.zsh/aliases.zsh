0fa4bbd198d5685ad840196e483a5d7b19189c21
Tyler Berry <tyler@berryac.com>
2021-04-08 18:25:43 -0600

    Zsh: Remove obsolete aliases

--- zsh/.zsh/aliases.zsh
+++ zsh/.zsh/aliases.zsh
@@ -1,19 +1,2 @@
-alias l='ls -CF'
-alias la='ls -A'
 alias ll='ls -alF'
-alias ls='ls --color=auto'
-
 alias vi=vim
-
-alias gcd='cd $(git rev-parse --show-toplevel)'
-alias gdt='git difftool'
-alias glu='git pull upstream $(current_branch)'
-
-alias grep='grep --color=auto'
-
-alias shutdown='\shutdown now'
-
-alias stow='\stow --target=$HOME *'
-
-alias k='kubectl'
-alias ki='kubectl -n istio-system'
