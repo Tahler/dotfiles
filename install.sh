14a0d02c8de86a9c51ed65eba5c2fabc44c986d4
Tyler Berry <tjberry@verily.com>
2024-12-16 03:52:19 -0800

    --wip-- [skip ci]

--- install.sh
+++ install.sh
@@ -21,8 +21,22 @@ install_if_not_exists ripgrep
 install_if_not_exists stow
 install_if_not_exists vim
 
-stow --target="$HOME" git unix vim vs-code zsh
+stow --target="$HOME" vim vs-code zsh
 
 "$HOME/.fzf/install" --all --no-fish
 
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+# if [ "$(uname -n)" == "penguin" ]; then
+#   keybindings=(
+#       "Super_L"
+#       "<Alt>bracketleft"
+#       "<Alt>bracketright"
+#       "<Alt>tab"
+#   )
+#   for k in "${keybindings[@]}"; do
+#     # vi /etc/systemd/user/sommelier*.service.d/cros-sommelier-{x-,}override.conf
+#     # and append ",<Alt>tab,<Control_L><Alt_L>comma,<Control_L><Alt_L>period"
+#     sudo tee -a /etc/systemd/user/sommelier*.service.d/cros-sommelier-{x-,}override.conf <<< "Environment=\"SOMMELIER_ACCELERATORS=$SOMMELIER_ACCELERATORS,$k\""
+#   done
+# fi
